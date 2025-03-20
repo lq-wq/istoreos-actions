f5a2fdc7cc2c4861fda5860d0a643c3d6036e240
lq-wq <61339326+lq-wq@users.noreply.github.com>
2025-03-20 13:29:34 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,6 +21,7 @@ git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/l
 # git clone https://github.com/lq-wq/luci-app-quickstart.git package/luci-app-quickstart
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome -b master
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/luci-app-lucky
+git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 git clone -b main https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
 git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp      
 git clone https://github.com/sbwml/luci-app-alist package/alist
